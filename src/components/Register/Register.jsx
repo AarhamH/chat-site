dc6bb0b95c95ec3586af6e045988c2a5a68899d5
AarhamH <aarham.haider@gmail.com>
2023-02-10 20:49:25 -0800

    Added index.css
    added premade index.css to have static color scheme and dimensions

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './register.css'
+
+const Register = () => {
+  return (
+    <div className='form_container'>
+          <div className='form_wrapper'>
+            <form>
+              <span className='logo'>Chat</span>
+              <span className='title'>Register</span>
+
+              <input type="text" placeholder='display name'/>
+              <input type="email" placeholder='email'/>
+              <input type="password" placeholder='password'/>
+              <input type="file" placeholder='file'/>
+              <button>Sign Up</button>
+            </form>
+            <p>Have an accout? Log In.</p>
+          </div>
+    </div>
+
+  )
+}
+
+export default Register
